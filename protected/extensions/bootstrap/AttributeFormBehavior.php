f8e32c0a55b38c7282ab8f2bc26a12474760f0ff
bluenight <novou.tran@live.fr>
2013-09-02 16:53:07 +0200

    Now it is possible to change the attribute behavior at the runtime.

--- protected/extensions/bootstrap/AttributeFormBehavior.php
+++ protected/extensions/bootstrap/AttributeFormBehavior.php
@@ -2,8 +2,15 @@
 
 class AttributeFormBehavior extends CModelBehavior
 {
+	public $bs_input_attribute;
+
 	public function getBusinessAttributes()
 	{
-		return new BsInputAttribute();
+		return $this->bs_input_attribute;
+	}
+
+	public function setBusinessAttributes(BsInputAttribute $input_attribute)
+	{
+		$this->bs_input_attribute = $input_attribute;
 	}
 }
