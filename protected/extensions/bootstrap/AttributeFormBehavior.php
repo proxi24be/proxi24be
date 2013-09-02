aeeecb3dac61f5d6ab1177be4aa57075b59c97ed
bluenight <novou.tran@live.fr>
2013-09-02 17:18:45 +0200

    adding some comments.

--- protected/extensions/bootstrap/AttributeFormBehavior.php
+++ protected/extensions/bootstrap/AttributeFormBehavior.php
@@ -4,6 +4,18 @@ class AttributeFormBehavior extends CModelBehavior
 {
 	public $bs_input_attribute;
 
+	/**
+	 * [initBusinessAttributes description]
+	 * This method should be override.  Ideally the configuration 
+	 * of the attributes should be done inside this function.
+	 * Do not forget to call it before call getBusinessAttributes otherwise
+	 * an error will rise up (depending if the instance has not been initialized)
+	 */
+	public function initBusinessAttributes()
+	{
+		// do nothing.
+	}
+
 	public function getBusinessAttributes()
 	{
 		return $this->bs_input_attribute;
