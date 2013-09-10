5591d68b0338442fb1fbc9662a6e4398257bbc3e
bluenight <novou.tran@live.fr>
2013-09-10 19:55:11 +0200

    helper class for crud controller.

---
+++ protected/extensions/helper/crud/CRUDController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ext\helper\crud;
+
+/**
+ *  This class performs generic CRUD operation.
+ */
+class CRUDController extends \Controller
+{
+    protected $_model_name;
+
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                'ips'=>array('127.0.0.1'),
+            ),
+
+            array('deny',  // deny all users
+                'users'=>array('*'),
+                'message'=>'Access Denied.',
+            ),
+        );
+    }
+
+    public function beforeAction($action)
+    {
+        if(!isset($this->_model_name))
+            throw new \CHttpException(501, 'The varialble model name has not been initialized !');
+        else
+            return parent::beforeAction($action);
+    }
+
+    public function actionRead()
+    {
+        $model = $this->_model_name;
+        $models = $model::model()->findAll();
+        echo \CJSON::encode($models);
+    }
+}
